dcd973fc484b66194bc47c9acecd6ab71c1e7215
Wanda Kalinowska <wanda0501@gmail.com>
2019-06-17 11:41:27 +0200

    add DN

---
+++ scripts/script.js
@@ -0,0 +1,10 @@
+$("#slideshow > div:gt(0)").hide();
+
+setInterval(function() {
+  $('#slideshow > div:first')
+    .fadeOut(3000)
+    .next()
+    .fadeIn(3000)
+    .end()
+    .appendTo('#slideshow');
+}, 7000);
